23d5c2d72a807a81ad8a6d9f0dbf10e7c2e1bd58
jihun-shim <heartrains1@gmail.com>
2024-10-08 17:03:00 +0900

    compare_lonas

---
+++ compare_loans.py
@@ -0,0 +1,29 @@
+'''
+-월금리 계산-
+신한은행 신한주택대출(아파트) : 0.005675
+신한은행 우리 WON 갈아타기 직장인대출 : 0.004308
+BNK BNK모바일주택담보대출 : 0.005441
+신한은행 신한 MY CAR 신차 대출 : 0.004508
+국민은행 KB 스타 아파트 담보대출 혼합금리(주택자금) : 0.003325
+주식회사 카카오뱅크 일반신용대출 : 0.003541
+신한은행 신한은행 쏠편한 직장인대출S I : 0.003975
+'''
+
+
+loan_products =[ 
+    {"company": "신한은행", "product": "신한주택대출(아파트)", "base_rate": 3.42, "additional_rate": 3.99, "preferred_rate": 0.60} ,'\n' ,
+    {"company": "우리은행", "product": "우리 WON 갈아타기 직장인대출", "base_rate": 3.97, "additional_rate": 2.60, "preferred_rate": 1.40}, '\n' ,
+    {"company": "BNK", "product": "BNK모바일주택담보대출", "base_rate": 3.14, "additional_rate": 3.99, "preferred_rate": 0.60},'\n' ,
+    {"company": "신한은행", "product": "신한 MY CAR 신차 대출", "base_rate": 3.41, "additional_rate": 2.60, "preferred_rate": 0.60},'\n' ,
+    {"company": "국민은행", "product": "KB 스타 아파트 담보대출 혼합금리(주택자금)", "base_rate": 3.15, "additional_rate": 2.24, "preferred_rate": 1.40},
+    {"company": "주식회사 카카오뱅크", "product": "일반신용대출", "base_rate": 3.32, "additional_rate": 1.53, "preferred_rate": 0.60} ,
+    {"company": "신한은행", "product": "신한은행 쏠편한 직장인대출S I", "base_rate": 4.87, "additional_rate": 1.30, "preferred_rate": 1.40}
+]
+
+'''
+print('-전체 상환 총액이 낮은 상품-')
+print('')
+ for i in loan_product:
+'''
+#
+print(loan_products)
---
+++ 백준/백준.py
@@ -0,0 +1,44 @@
+# a,b,c = map(int, input().split())
+# d,e,f = map(int, input().split())
+# print((a,b,c)*f)
+# print((a,b,c)*e)
+# print((a,b,c)*d)
+
+
+# a,b,c = map(int, input().split())
+# d,e,f = map(int, input().split())
+# g=print(0, 0, 0, a * (f), b * (f), c * (f))
+# h=print(0, a * (e), b * (e), c * (e), 0)
+# i=print(a * (d), b * (d), c * (d), 0, 0)
+# print('g'+'h')
+
+# a = int(input())
+# b = int(input())
+
+# b_str = str(b)
+# print(a*int(b_str[2])) 
+# print(a*int(b_str[1]))     
+# print(a*int(b_str[0])) 
+# print(a*b)
+
+# a, b, c = map(int, input().split())
+# print(a+b+c)
+'''
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+'''
+#'\ '  '  '  '  '  '/''\ ''\n''|'')''  ''('
+
+# a='\ ''   ''/''\ ''\n'' )''  ''('' '"'"')''\n''(  ' '/' '  )''\n'' \(__)''|'
+# print(a)
+'''
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+'''
+a = '|\_/|''\n' '|q p|   /}''\n' '( 0 )"""\ ''\n' '|"^"`    |''\n' '||_/=\\__|' '\n'
+print(a)
